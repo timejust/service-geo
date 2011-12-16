24467a5416d80c39e307165f4f50aff5b53f7f24
minsikzzang <minsikzzang@hotmail.com>
2011-12-16 19:37:14 +0100

    added missing file

---
+++ src/main/scala/com/timejust/util/Datetime.scala
@@ -0,0 +1,32 @@
+package com.timejust.util
+
+import java.util.Calendar
+import java.text.SimpleDateFormat
+
+object Datetime {
+  def unixToDateString(unix: String) = {
+    val c = Calendar.getInstance()
+    if (unix == null) {
+      c.setTimeInMillis(System.currentTimeMillis())  
+    } else {
+      // If the given unix time is null, get current time stamp
+      c.setTimeInMillis(unix.toLong * 1000)  
+    }          
+
+    val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    sdf.format(c.getTime())      
+  }  
+  
+  def unixToDateString(unix: Long) = {
+    val c = Calendar.getInstance()
+    if (unix == null) {
+      c.setTimeInMillis(System.currentTimeMillis())  
+    } else {
+      // If the given unix time is null, get current time stamp
+      c.setTimeInMillis(unix * 1000)  
+    }          
+
+    val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    sdf.format(c.getTime())      
+  }  
+}
